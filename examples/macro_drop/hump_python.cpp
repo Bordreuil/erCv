fee594df06e52e2189ad58945512d53e2a7316a4
Cyril Bordreuil <cyril.bordreuil@univ-montp2.fr>
2012-06-27 12:10:57 +0200

    correction

--- examples/macro_drop/hump_python.cpp
+++ examples/macro_drop/hump_python.cpp
@@ -4,7 +4,7 @@ int main( int hola, char** file_name)
 {
   CvRect rect;
   rect.x=121;rect.y=110;rect.width=814;rect.height=223;
-  erCerc cerc(123,293,17);
+  erCerc cerc(135,293,25);
   erEqualP equ(0);
   erSmootP p1(BLUR,7),p2(MEDIAN,5);
   erCannyP cann(355,355);
